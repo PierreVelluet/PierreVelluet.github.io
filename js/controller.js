46236a710f6ea782085eec3905f1615b78f5383c
Pierre Velluet <pierre.velluet@willow.fr>
2021-06-08 16:19:27 +0400

    first part almost done

---
+++ js/controller.js
@@ -0,0 +1,16 @@
+
+import languages from '../js/locales.js';
+
+const locale = "fr";
+
+document.getElementById("mainTitle").innerHTML = languages[locale].mainTitle;
+document.getElementById("homeTab").innerHTML = languages[locale].homeTab;
+document.getElementById("aboutTab").innerHTML = languages[locale].aboutTab;
+document.getElementById("serviceTab").innerHTML = languages[locale].serviceTab;
+document.getElementById("experienceTab").innerHTML = languages[locale].experienceTab;
+document.getElementById("portfolioTab").innerHTML = languages[locale].portfolioTab;
+document.getElementById("contactTab").innerHTML = languages[locale].contactTab;
+document.getElementById("iAm").innerHTML = languages[locale].iAm;
+document.getElementById("myName").innerHTML = languages[locale].myName;
+document.getElementById("myJobs").innerHTML = languages[locale].myJobs;
+document.getElementById("contactBtn").innerHTML = languages[locale].contactBtn;
---
+++ js/locales.js
@@ -0,0 +1,20 @@
+const languages = {
+    fr: {
+        mainTitle: "La fabrique numérique de Peyo ®",
+        homeTab: 'Accueil',
+        aboutTab: "A propos",
+        serviceTab: "Compétences",
+        experienceTab: "Experience",
+        portfolioTab: "Portfolio",
+        contactTab: "Contact",
+        iAm: "Je m'appelle",
+        myName: "Pierre Velluet",
+        myJobs: "Développeur web, Stack Javascript complète, Stack Microsoft C# .Net",
+        contactBtn: "Me contacter"
+    },
+    en: {
+        mainTitle: "Peyo's digital factory ®",
+    }
+}
+
+export default languages;
